bb55aa5fef15582f4693566fe816ef6a30655052
Asliddin <saydaliev.asliddin@mail.ru>
2024-08-29 00:21:29 +0500

    fix

--- lib/ui/components/about_me.dart
+++ lib/ui/components/about_me.dart
@@ -11,32 +11,30 @@ class AboutMeWidget extends StatelessWidget {
     return AppContainer(
       height: UiUtils.isMobileDevice(context) ? 350 : 250,
       child: const Padding(
-        padding: EdgeInsets.all(10),
-        child: Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              AppLinkText(
-                'Участник хакатона от Университета Иннополис',
-              ),
-              AppLinkText(
-                'Участник в югорском хакатоне "Хантатон"',
-              ),
-              AppLinkText(
-                'Призер в global management challenge, (https://www.ugrasu.ru/news/extracurricular-activities/studenty-yugu-v-chisle-luchshikh-v-chempionate-po-strategii-i-upravleniyu-biznesom-global-management/)',
-              ),
-              AppLinkText(
-                'Участник программы студенческих стартапов "УМНИК", (https://www.tp86.ru/press-centr/news/39342/)',
-              ),
-              AppLinkText(
-                'Участник акселератора ПУТП 2022',
-              ),
-              AppLinkText(
-                'Призер (диплом 2 степени) конференции «Информационные технологии Югры», (https://www.ugrasu.ru/news/news/v-yugu-proshla-konferentsiya-informatsionnye-tekhnologii-yugry/)',
-              ),
-            ],
-          ),
+        padding: EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            AppLinkText(
+              'Участник хакатона от Университета Иннополис',
+            ),
+            AppLinkText(
+              'Участник в югорском хакатоне "Хантатон"',
+            ),
+            AppLinkText(
+              'Призер в global management challenge, (https://www.ugrasu.ru/news/extracurricular-activities/studenty-yugu-v-chisle-luchshikh-v-chempionate-po-strategii-i-upravleniyu-biznesom-global-management/)',
+            ),
+            AppLinkText(
+              'Участник программы студенческих стартапов "УМНИК", (https://www.tp86.ru/press-centr/news/39342/)',
+            ),
+            AppLinkText(
+              'Участник акселератора ПУТП 2022',
+            ),
+            AppLinkText(
+              'Призер (диплом 2 степени) конференции «Информационные технологии Югры», (https://www.ugrasu.ru/news/news/v-yugu-proshla-konferentsiya-informatsionnye-tekhnologii-yugry/)',
+            ),
+          ],
         ),
       ),
     );
